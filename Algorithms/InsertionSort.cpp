6f2a51e7a7befc050f0f7533b2687f021a52f1b0
Mohit Kumar <82509454+mohitkr07@users.noreply.github.com>
2022-10-20 11:44:56 +0530

    insertionSort

---
+++ Algorithms/InsertionSort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int> &nums)
+{
+    int n = nums.size();
+    int key;
+    int j;
+    for (int i = 1; i < n; i++)
+    {
+        key = nums[i];
+        j = i - 1;
+
+        while (nums[j] > key)
+        {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+        nums[j + 1] = key;
+    }
+}
+void printArray(vector<int> &nums)
+{
+    for (auto it : nums)
+    {
+        cout << it << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    insertionSort(nums);
+    printArray(nums);
+
+    return 0;
+}
